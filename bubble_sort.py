cdea5253c22731602b907e295157fa32c61ece22
Diego <diegohurtado.esteve@gmail.com>
2020-07-24 13:03:35 +0200

    Removed all commented out parts.

--- bubble_sort.py
+++ bubble_sort.py
@@ -15,11 +15,8 @@ def bubble(data):
             if i==len(data)-1:
                 pass
             elif i<len(data)-1:
-                # print(data[i], data[y])
                 if data[i]>data[y]:
-                    # print("swapping")
                     data[i],data[y]=data[y],data[i]
-                    # print(data)
 
                 yield i
 
