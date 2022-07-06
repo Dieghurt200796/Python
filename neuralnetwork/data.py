c251a6a170dc6cf375bb7c2d333475b2647ec622
Diego <dieghurt200796@gmail.com>
2022-07-06 13:10:31 +0200

    made non working animation, started neural network

--- neuralnetwork/data.py
+++ neuralnetwork/data.py
@@ -1,11 +1,13 @@
-import matplotlib.pyplot as plt, numpy as np, random
+import matplotlib.pyplot as plt, numpy as np, random, itertools
 
 
 def classify(point, line):
+    c = 0
     above_line = 0
     line_gradient = line[1][1] / line[1][0]
+    line_y = line_gradient * point[0] + c
     point_gradient = point[1] / point[0]
-    if point_gradient > line_gradient:
+    if point[1] > line_y:
         above_line = 1
     return above_line
 
@@ -17,18 +19,20 @@ def label_points(points, line):
     return labelled_array
 
 def create_points(amount=100):
-    points = np.random.random(size = (amount, 2)) * 98 + 1 # Add 1000 different random coordinates
+    points = np.random.random(size = (amount, 2)) * 200 - 100 # Add 1000 different random coordinates
     return points
 
 def create_line():
-    line = np.array([(0,0), (100, random.random()* 80 + 20)]) # Start and end coordinates of the line. Line is always at x = 100 or y = 100, the other coordinate is 20 + a random number between 0 and 80 (20 is a lower bound buffer)
-    if random.randint(0, 100) % 2: # Deciding whether the x or the y of the line's end is 100.
-        line[1][::-1] # If y has been chosen, flipping the array's 1st index to make the end of y 100.
+    fixed = random.choice([-100,100])
+    line = np.array([(0,0), (fixed, random.random()* 200 - 100)]) # Start and end coordinates of the line. Line is always at x = 100 or y = 100, the other coordinate is 20 + a random number between 0 and 80 (20 is a lower bound buffer)
+    if random.randint(0, 100) % 2:
+        line[1][::-1] # If y has been chosen, flipping the array's 1st index
     return line
 
 def draw(line):
     plt.style.use("ggplot") #Changing colour of points and line
     plt.figure(figsize=(5,5)) # Makes the graph a square, which is rectangular by default
+    gathered_points = []
     while True:
         points = yield
         if points is None:
@@ -38,11 +42,24 @@ def draw(line):
             colours = [("blue" if label == 1 else "green") for label in points_array[:, 2]]
         else:
             colours = "red"
-        plt.scatter(points_array[:,0], points_array[:,1], s=1, c = colours) # Plotting the points
-    plt.plot(line[:,0],line[:,1]) # Plotting the line
+        #plt.scatter(points_array[:,0], points_array[:,1], s=1, c = colours) # Plotting the points
+        gathered_points.append([points_array[:,0], points_array[:,1], 1, colours])
+    if line is not None:
+        plt.plot(line[:,0],line[:,1]) # Plotting the line
+        plt.plot(line[:,0] * -1, line[:,1] * -1, ls=':') # Plotting the dotted line  which continues the line
+    for points_set in gathered_points:
+        xs, ys, sizes, colours = points_set
+        if type(colours) is list:
+            args = np.array([xs, ys, colours]).T
+        else:
+            args = np.array([xs, ys, itertools.repeat(colours)]).T
+        for i in range(len(args)):
+            plt.scatter(args[:i,0].astype(float), args[:i,1].astype(float), s = 1, c = args[:i,2])
+            if i % 50 == 0:
+                plt.pause(0.01)
     plt.show() # Displaying the graph
 
-def display(*points, line):
+def display(*points, line=None):
     try:
         # Initialising the generator:
         displayer = draw(line)
--- neuralnetwork/nn.py
+++ neuralnetwork/nn.py
@@ -1,4 +1,4 @@
-import data
+import data, random
 
 if __name__ == "__main__":
     # Creating the line and data for the points.
@@ -7,5 +7,29 @@ if __name__ == "__main__":
     testing_data = data.create_points(500)
 
     # Displaying all the data:
-    data.display(training_data, testing_data, line=line)
+    # data.display(training_data, testing_data, line=line)º
 
+
+    weights = [random.random() * 2 - 1, random.random() * 2 - 1]
+    biases = [1, 1]
+
+    def step_activation(value):
+        if value > 0:
+            return 1
+        else:
+            return 0 
+
+    def feed_forward(point):
+        output = point[0] * weights[0] * biases[0] + point[1] * weights[1] * biases[1]
+        activation = step_activation(output)
+        return activation
+
+    guesses = []
+    for point in testing_data:
+        activation = feed_forward(point / 100)
+        guesses.append([*point, activation])
+    
+    
+    #data.display(guesses)
+    data.display(guesses, line=line)
+    data.display(training_data, line=line)
