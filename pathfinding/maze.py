f508f882bdc1847580c8f7a3200e5e857df42e90
Dieghurt200796 <68736801+Dieghurt200796@users.noreply.github.com>
2022-07-14 13:42:33 +0200

    Update maze.py

--- pathfinding/maze.py
+++ pathfinding/maze.py
@@ -1,70 +1,163 @@
+import os
+import random
+
 import svgwrite as svg
 from svgwrite import percent as pct
 
-class Display:
-    def __init__(self, rows, columns, padding):
-        self.rows = rows
-        self.columns = columns
-        self.padding = padding
-        self.drawing = svg.Drawing("pathfinding/image.svg", size = (800, 800))
-        self.drawing.add(self.drawing.rect((0,0), (1200,1200), fill="white"))
-        print(f"Creating an svg with {self.rows} rows, {self.columns} columns.")
 
-    def draw_line(self, start, end):
-        self.drawing.add(self.drawing.line(
+
+class Display:
+    def __init__(self, maze, padding):
+        self.maze = maze
+        self.rows = maze.rows
+        self.columns = maze.columns
+        self.padding = padding
+        self.filename = "pathfinding/maze.svg"
+        self.drawing = svg.Drawing(self.filename, size=(100*pct, 100*pct))
+        self.drawing.add_stylesheet("styles.css", title="stylesheet")
+
+        bg = self.drawing.rect((0*pct, 0*pct), (100*pct, 100*pct), fill="navy")
+        # bg.add(self.drawing.animate("fill", values=["red", "green", "blue", "green", "red"], begin="0s", dur="5s", repeatCount="indefinite"))
+        self.drawing.add(bg)
+
+        print(f"Creating svg with {self.rows} rows, {self.columns} columns.")
+
+    def draw_line(self, start, end, lifetime=None):
+        line = self.drawing.line(
             (start[0]*pct, start[1]*pct),
             (end[0]*pct, end[1]*pct),
-            stroke="red", stroke_width=5, stroke_linecap="round"
-        ))
+            stroke="white", stroke_width=4, stroke_linecap="square"
+        )
+        if lifetime:
+            time = f"{lifetime}s"
+            line.add(self.drawing.animate("opacity", values=[1, 0], begin=time, dur="0.25s", fill="freeze"))
+        self.drawing.add(line)
+    
+    def draw_square(self, start, appears, disappears):
+        rect = self.drawing.rect(
+            ((start[0] + self.cell_width * 0.2) *pct, (start[1] + self.cell_height * 0.2) *pct),
+            (self.cell_width * 0.6 * pct, self.cell_height  * 0.6 * pct),
+            fill="orange", opacity=0
+        )
+        time = f"{appears}s"
+        end = f"{disappears}s"
+        rect.add(self.drawing.animate("opacity", values=[0, 1], begin=time, dur="0.25s", fill="freeze"))
+        rect.add(self.drawing.animate("opacity", values=[1, 0], begin=end, dur="0.25s", fill="freeze"))
+        self.drawing.add(rect)
 
     def draw_grid(self):
-        cell_width = (100 - self.padding * 2) / self.columns
-        cell_height = (100 - self.padding * 2) / self.rows
+        self.cell_width = (100 - self.padding * 2) / self.columns
+        self.cell_height = (100 - self.padding * 2) / self.rows
+
+        walls_removed = list(self.maze.generator)
+
         for row in range(self.rows):
             for column in range(self.columns):
-                x = self.padding + cell_width * column
-                y = self.padding + cell_height * row
+                x = self.padding + self.cell_width * column
+                y = self.padding + self.cell_height * row
+                cell = self.maze.grid[row][column]
                 top_left = x, y
-                top_right = x + cell_width, y
-                bottom_left = x, y + cell_height
-                bottom_right = x + cell_width, y + cell_height
-                self.draw_line(top_left, top_right)
-                self.draw_line(top_right, bottom_right)
-                self.draw_line(bottom_right, bottom_left)
-                self.draw_line(bottom_left, top_left)
-        self.drawing.save()
+                top_right = x + self.cell_width, y
+                bottom_left = x, y + self.cell_height
+                bottom_right = x + self.cell_width, y + self.cell_height
+                
+                line_data = zip(
+                    ("up", "right", "down", "left"),
+                    (top_left, top_right, bottom_right, bottom_left),
+                    (top_right, bottom_right, bottom_left, top_left)
+                )
+                
+                for direction, start, end in line_data:
+                    if not cell[direction]:
+                        for i, walls in enumerate(walls_removed):
+                            if len(walls) == 1 and (column, row) in walls:
+                                death = 5 / len(walls_removed) * i + 1
+                            if len(walls) > 1 and (column, row, direction) in walls:
+                                lifetime = 5 / len(walls_removed) * i + 1
+                        self.draw_square(top_left, lifetime, death)
+                    else:
+                        lifetime = None
+                    self.draw_line(start, end, lifetime)
+
+        self.drawing.save(pretty=True)
+        print("Saved svg to", os.path.abspath(self.filename))
+
+
 
 class Maze:
+    directions = ["up", "down", "left", "right"]
+    opposites = {"up": "down", "down": "up", "left": "right", "right": "left"}
+
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
-        self.grid = [[{"up" :  True, "down" : True, "left" : True, "right" : True, "visited" : False} for _ in range(self.columns)] for _ in range(self.rows)]
+        self.grid = [
+            [{"up": True, "down": True, "left": True, "right": True, "visited": False}
+                for _ in range(self.columns)]
+                    for _ in range(self.rows)
+        ]
         self.stack = [(self.columns // 2, self.rows // 2)]
-
+        self.generator = self.generate()
+        
     def step(self):
         # If the stack contains a cell, pop and use as current
         if self.stack:
-            x, y = self.stack.pop(0)
+            x, y = self.stack.pop()
             cell = self.grid[y][x]
-            
-            up = self.grid[y-1][x]
-            down = self.grid[y+1][x]
-            left = self.grid[y][x-1]
-            right = self.grid[y][x+1]
+            cell["visited"] = True
+
+            # Find all unvisited neighbours of current cell
+            up = x, y - 1
+            down = x, y + 1
+            left = x - 1, y
+            right = x + 1, y
 
             neighbours = []
-            for neighbour in [up, down, left, right]:
-                if neighbour["visited"] == False:
-                    neighbours.append(neighbour)
+            for vector, direction in zip([up, down, left, right], Maze.directions):
+                if self.valid_cell(*vector):
+                    neighbour = self.grid[vector[1]][vector[0]]
+                    neighbours.append((neighbour, direction))
 
-        # Find all unvisited neighbours of current cell
+            if neighbours:
+                # If at least one neighbour, pick one randomly, and remove walls between
+                next_cell, direction = random.choice(neighbours)
+                # Removes the wall from the current cell
+                opposite = Maze.opposites[direction]
+                cell[direction] = False
+                next_cell[opposite] = False
 
-        # If at least one neighbour, pick one randomly
+                if direction == "up": next_position = (x, y - 1)
+                if direction == "down": next_position = (x, y + 1)
+                if direction == "left": next_position = (x - 1, y)
+                if direction == "right": next_position = (x + 1, y)
+                
+                # Add current position to the stack, repeat
+                self.stack.append((x, y))
+                self.stack.append(next_position)
 
-        # Add current posiition to the stack, repeat
+                return (x, y, direction), (*next_position, opposite)
+            else:
+                # Run when in dead-end, no possible neighbours
+                return (x, y),
 
-        pass
+    def valid_cell(self, x, y):    # Predicate function
+        # Check if y is between 0 and number of rows
+        # Check if x is between 0 and number of columns
+        # If so, then check if the cell has been visited
+        return 0 <= x < self.columns and 0 <= y < self.rows and self.grid[y][x]["visited"] == False
+    
+    def generate(self):
+        self.grid[0][0]["left"] = False
+        self.grid[-1][-1]["right"] = False
+        while self.stack:
+            walls = self.step()
+            yield walls
+
 
 if __name__ == "__main__":
-    Display(11,11,10).draw_grid()
-    Maze(10, 10)
+    maze = Maze(15, 15)
+    try:
+        Display(maze, 10).draw_grid()
+    except:
+        print("Run code again for an enhanced experience. An error from your computer's side was detected. We will attempt to adjust this automatically; if this error persists, try rebooting your device.")
+    
